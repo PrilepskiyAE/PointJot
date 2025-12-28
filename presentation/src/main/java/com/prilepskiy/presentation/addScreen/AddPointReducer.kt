2fd89c67fca4c8f95cec007ddc5cbec832e1e8e8
Alex92w <woolf7754286@yandex.ru>
2025-12-28 17:34:48 +0700

    impl category logic

---
+++ presentation/src/main/java/com/prilepskiy/presentation/addScreen/AddPointReducer.kt
@@ -0,0 +1,5 @@
+package com.prilepskiy.presentation.addScreen
+
+class AddPointReducer{
+
+}
---
+++ presentation/src/main/java/com/prilepskiy/presentation/addScreen/AddPointScreen.kt
@@ -0,0 +1,8 @@
+package com.prilepskiy.presentation.addScreen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddPointScreen(point: Int?, onPopBack: () -> Unit) {
+
+}
